2e72d509ad4650dc3f89703fdec504e991bdd63a
ghizlanenory01-crypto <ghizlanenory01@gmail.com>
2026-06-24 00:14:16 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Competitor Analysis App")
+
+st.write("This app allows users to search for AI mental health apps and visualize results.")
+
+st.header("How to run")
+st.code("streamlit run Home.py")
